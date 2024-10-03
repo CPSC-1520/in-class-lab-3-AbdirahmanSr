739f187da11782d067d2572a4ef85525b38aeda2
Abdirahman  Ali <143823228+AbdirahmanSr@users.noreply.github.com>
2024-10-02 23:42:57 -0400

    comments added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 // Enter your code below.
 
+//show the recourses div when button is clicked
 const resourcesButton = document.getElementById('show-resources');
 const resDiv = document.querySelector('.javascript-resources');
 
 resourcesButton.addEventListener('click', function() {
-    
+   
     resDiv.classList.remove('d-none');
 
 });
 
+//add bold font when mouse hovers over list items
 resDiv.addEventListener('mouseover', function(event) {
     console.log(event.target);
     
@@ -18,6 +20,7 @@ resDiv.addEventListener('mouseover', function(event) {
 
 });
 
+//remove the font weight when we dont hover anymore
 resDiv.addEventListener('mouseout', function(event) {
     
     if (event.target.classList.contains('list-group-item')) {
@@ -26,6 +29,7 @@ resDiv.addEventListener('mouseout', function(event) {
 
 });
 
+//add the font style when list item is clicked
 resDiv.addEventListener('click', function(event) {
     
     if (event.target.classList.contains('list-group-item')) {
