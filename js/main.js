1bb84b275a36cfaf6b9c7f9f0ea5236ee0fa6d28
Abdirahman  Ali <143823228+AbdirahmanSr@users.noreply.github.com>
2024-10-02 23:32:30 -0400

    readability

--- js/main.js
+++ js/main.js
@@ -4,24 +4,32 @@ const resourcesButton = document.getElementById('show-resources');
 const resDiv = document.querySelector('.javascript-resources');
 
 resourcesButton.addEventListener('click', function() {
+    
     resDiv.classList.remove('d-none');
+
 });
 
 resDiv.addEventListener('mouseover', function(event) {
     console.log(event.target);
+    
     if (event.target.classList.contains('list-group-item')) {
         event.target.classList.add('fw-bold');
     }
+
 });
 
 resDiv.addEventListener('mouseout', function(event) {
+    
     if (event.target.classList.contains('list-group-item')) {
         event.target.classList.remove('fw-bold');
     }
+
 });
 
 resDiv.addEventListener('click', function(event) {
+    
     if (event.target.classList.contains('list-group-item')) {
         event.target.classList.add('fst-italic');
     }
+
 });
