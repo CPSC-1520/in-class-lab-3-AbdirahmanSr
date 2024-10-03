a64b334557a7a53672e8e64f0c9eb722d2cbb0c2
Abdirahman  Ali <143823228+AbdirahmanSr@users.noreply.github.com>
2024-10-02 23:21:31 -0400

    main.js updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 // Enter your code below.
+
+const showResourcesButton = document.getElementById('show-resources');
+const resourcesDiv = document.querySelector('.javascript-resources');
+
+showResourcesButton.addEventListener('click', function() {
+    resourcesDiv.classList.remove('d-none');
+});
+
+resourcesDiv.addEventListener('mouseover', function(event) {
+    console.log(event.target);
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.add('fw-bold');
+    }
+});
+
+resourcesDiv.addEventListener('mouseout', function(event) {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.remove('fw-bold');
+    }
+});
+
+resourcesDiv.addEventListener('click', function(event) {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.add('fst-italic');
+    }
+});
